70bcb5d823994342ff03e7182492ff8a42d6ef7f
Roni G <Zontex@users.noreply.github.com>
2019-12-05 18:03:15 +0700

    Create servo.py

---
+++ examples/servo.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author : Original author WindVoiceVox
+# Original Author Github: https://github.com/WindVoiceVox/Raspi_SG90
+# http://elecrow.com/
+
+import RPi.GPIO as GPIO
+import time
+import sys
+
+class sg90:
+
+  def __init__( self, direction):
+
+    self.pin = 25
+    GPIO.setmode( GPIO.BCM )
+    GPIO.setup( self.pin, GPIO.OUT )
+    self.direction = int( direction )
+    self.servo = GPIO.PWM( self.pin, 50 )
+    self.servo.start(0.0)
+
+  def cleanup( self ):
+
+    self.servo.ChangeDutyCycle(self._henkan(0))
+    time.sleep(0.3)
+    self.servo.stop()
+    GPIO.cleanup()
+
+  def currentdirection( self ):
+
+    return self.direction
+
+  def _henkan( self, value ):
+
+    return 0.05 * value + 7.0
+
+  def setdirection( self, direction, speed ):
+
+    for d in range( self.direction, direction, int(speed) ):
+      self.servo.ChangeDutyCycle( self._henkan( d ) )
+      self.direction = d
+      time.sleep(0.1)
+    self.servo.ChangeDutyCycle( self._henkan( direction ) )
+    self.direction = direction
+
+def main():
+
+    s = sg90(0)
+
+    try:
+        while True:
+            print("Turn left ...")
+            s.setdirection( 100, 80 )
+            time.sleep(0.5)
+            print("Turn right ...")
+            s.setdirection( -100, 80 )
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        s.cleanup()
+
+if __name__ == "__main__":
+    main()
