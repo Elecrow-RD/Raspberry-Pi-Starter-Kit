ca52972598b941095c7fd14bb71c78d567b76585
Roni G <Zontex@users.noreply.github.com>
2019-12-09 15:52:30 +0700

    Update pcf8591.py

--- examples/pcf8591.py
+++ examples/pcf8591.py
@@ -4,18 +4,22 @@
 import smbus
 import time
 
+
 # NOTE: the PCF8591 script can be used both with photoresistance example and flame sensor example
 # please follow up the instructions on the PCF8591 schematic to connect the right sensors in the right way.
 
 address = 0x48
-cmd = 0x40
-value = 0
+
+A0 = 0x40
+A1 = 0x41
+A2 = 0x42
+A3 = 0x43
 
 bus = smbus.SMBus(1)
+
 while True:
-    bus.write_byte_data(address,cmd,value)
-    value += 1
-    if value == 256:
-        value =0
-    print("AOUT:%3d" %value)
-    time.sleep(0.01)
+    bus.write_byte(address,A0)
+    value = bus.read_byte(address)
+    print("AOUT:%1.3f  " %(value*3.3/255))
+    time.sleep(0.1)
+    
